d982c351bdd5cf9d002f125fab4339ab608e632c
bloshound <bloshound@gmail.com>
2021-01-15 21:14:53 +0300

    new home commit. refactor code. del spinner -> use number picker

---
+++ app/src/main/java/ru/bloshound/electricalbusbars/ui/main/BusbarHolderFragment.java
@@ -0,0 +1,57 @@
+package ru.bloshound.electricalbusbars.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import ru.bloshound.electricalbusbars.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BusbarHolderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private BusbarViewModel busbarViewModel;
+
+
+
+    public static BusbarHolderFragment newInstance(int index) {
+        BusbarHolderFragment fragment = new BusbarHolderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        busbarViewModel = new ViewModelProvider(this).get(BusbarViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        busbarViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final TextView textView = (TextView) root.findViewById(R.id.section_label);
+
+        busbarViewModel.getText().observe(this, s -> textView.setText(s));
+        return root;
+    }
+
+}
