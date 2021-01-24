f09a9f136a15779aeb3a831f9b33dec4d7dde598
bloshound <bloshound@gmail.com>
2021-01-25 01:30:37 +0300

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	app/src/main/java/ru/bloshound/electricalbusbars/ui/fragment/BusbarHolderFragment.java

--- app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
+++ app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import ru.bloshound.electricalbusbars.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
