58ddaf780503d7d537e5bcd753a102e65914774b
bloshound <bloshound@gmail.com>
2021-01-27 01:35:10 +0300

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	app/src/main/java/ru/bloshound/electricalbusbars/ui/fragment/BusbarHolderFragment.java

--- app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
+++ app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
@@ -1,19 +1,106 @@
 package ru.bloshound.electricalbusbars;
 
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText quantuty_ed;
+    Slider quantity_slider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+
+        quantuty_ed = findViewById(R.id.ed_quantity);
+        quantity_slider = findViewById(R.id.slider_quantity);
+
+        quantuty_ed.addTextChangedListener(getMinMaxEditTextWatcher
+                (resources.getInteger(R.integer.min_value),
+                        resources.getInteger(R.integer.quantity_max_value)));
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
     }
+
+
+    private TextWatcher getMinMaxEditTextWatcher(int min, int max) {
+        TextWatcher textWatcher = new TextWatcher() {
+
+            CharSequence ch = "";
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.isEmpty(s) && TextUtils.isDigitsOnly(s)) {
+                    int value = Integer.parseInt(s.toString());
+
+                    if (value > max) {
+                        s.replace(0, s.length(), String.valueOf(max));
+                    }
+                    if (value < min) {
+                       s.replace(0, s.length(), String.valueOf(min));
+                    }
+
+
+
+
+
+                }
+
+
+            }
+        };
+        return textWatcher;
+    }
+
+
+    private TextWatcher getSliderSetValueTextWatcher(Slider slider) {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        };
+        return textWatcher;
+    }
+
 }
--- app/src/main/java/ru/bloshound/electricalbusbars/SharedPreferencesHelper.java
+++ app/src/main/java/ru/bloshound/electricalbusbars/SharedPreferencesHelper.java
@@ -2,12 +2,10 @@ package ru.bloshound.electricalbusbars;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.widget.ArrayAdapter;
+
 
 import com.google.gson.Gson;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class SharedPreferencesHelper {
     public static final String SHARED_PREF_NAME = "shared_pref_name";
@@ -21,7 +19,7 @@ public class SharedPreferencesHelper {
     public SharedPreferencesHelper(Context context) {
         this.mSharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
 
-        ArrayList arrayList = new ArrayList();
+
 
     }
 }
