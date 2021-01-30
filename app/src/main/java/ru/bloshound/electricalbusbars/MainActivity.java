27e1de22fdda2e4f666afed1725ecd499fe4fc6f
bloshound <bloshound@gmail.com>
2021-01-31 01:49:56 +0300

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	app/src/main/java/ru/bloshound/electricalbusbars/ui/fragment/BusbarHolderFragment.java

--- app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
+++ app/src/main/java/ru/bloshound/electricalbusbars/MainActivity.java
@@ -23,16 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
     private AutoCompleteTextView mMaterail_autotv;
     private EditText mQuantity_ed;
+    private EditText mLength_ed;
+    private EditText mWidth_ed;
+    private EditText mThickness_ed;
+
     private Slider mQuantity_slider;
-
-
-    private MinMaxEditTextWatcher mQuantityMinMaxInput;
-    private SliderAfterChangeTextWatcher mQuantityInputWatcher;
-    private Slider.OnChangeListener mQuantitySliderListener;
+    private Slider mLength_slider;
+    private Slider mWidth_slider;
+    private Slider mThickness_slider;
 
     private View.OnFocusChangeListener mOnMaterialFocusChangeListener = (v, hasFocus) -> {
         if (hasFocus) mMaterail_autotv.showDropDown();
     };
+
+    private MinMaxEditTextWatcher mQuantityMinMaxInput;
+    private SliderAfterChangeTextWatcher mQuantityInputWatchSlider;
+    private Slider.OnChangeListener mQuantitySliderListener;
+
+    private MinMaxEditTextWatcher mLengthMinMaxInput;
+    private SliderAfterChangeTextWatcher mLengthInputWatchSlider;
+    private Slider.OnChangeListener mLengthSliderListener;
+
+    private MinMaxEditTextWatcher mWidthMinMaxInput;
+    private SliderAfterChangeTextWatcher mWidthInputWatchSlider;
+    private Slider.OnChangeListener mWidthSliderListener;
+
+    private MinMaxEditTextWatcher mThicknessMinMaxInput;
+    private SliderAfterChangeTextWatcher mThicknessInputWatchSlider;
+    private Slider.OnChangeListener mThicknessSliderListener;
 
 
     @Override
@@ -45,7 +63,15 @@ public class MainActivity extends AppCompatActivity {
         mMaterail_autotv = findViewById(R.id.actv_chosen_material);
 
         mQuantity_ed = findViewById(R.id.ed_quantity);
+        mLength_ed = findViewById(R.id.ed_length);
+        mWidth_ed = findViewById(R.id.ed_width);
+        mThickness_ed = findViewById(R.id.ed_thickness);
+
         mQuantity_slider = findViewById(R.id.slider_quantity);
+        mLength_slider = findViewById(R.id.slider_length);
+        mWidth_slider = findViewById(R.id.slider_width);
+        mThickness_slider = findViewById(R.id.slider_thickness);
+
 
         mMaterialAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line,
@@ -55,8 +81,24 @@ public class MainActivity extends AppCompatActivity {
 
         mQuantityMinMaxInput = new MinMaxEditTextWatcher(getResources().getInteger(R.integer.min_value),
                 getResources().getInteger(R.integer.quantity_max_value));
-        mQuantityInputWatcher = new SliderAfterChangeTextWatcher(mQuantity_slider);
+        mLengthMinMaxInput = new MinMaxEditTextWatcher(getResources().getInteger(R.integer.min_value),
+                getResources().getInteger(R.integer.length_max_value));
+        mWidthMinMaxInput = new MinMaxEditTextWatcher(getResources().getInteger(R.integer.min_value),
+                getResources().getInteger(R.integer.width_max_value));
+        mThicknessMinMaxInput = new MinMaxEditTextWatcher(getResources().getInteger(R.integer.min_value),
+                getResources().getInteger(R.integer.thickness_max_value));
+
+
+        mQuantityInputWatchSlider = new SliderAfterChangeTextWatcher(mQuantity_slider);
+        mLengthInputWatchSlider = new SliderAfterChangeTextWatcher(mLength_slider);
+        mWidthInputWatchSlider = new SliderAfterChangeTextWatcher(mWidth_slider);
+        mThicknessInputWatchSlider = new SliderAfterChangeTextWatcher(mThickness_slider);
+
+
         mQuantitySliderListener = (slider, value, fromUser) -> mQuantity_ed.setText(String.valueOf((int) value));
+        mLengthSliderListener = (slider, value, fromUser) -> mLength_ed.setText(String.valueOf((int) value));
+        mWidthSliderListener = (slider, value, fromUser) -> mWidth_ed.setText(String.valueOf((int) value));
+        mThicknessSliderListener = (slider, value, fromUser) -> mThickness_ed.setText(String.valueOf((int) value));
 
         setHints();
 
@@ -66,12 +108,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        mMaterail_autotv.setOnFocusChangeListener(mOnMaterialFocusChangeListener);
+        setWatchersAndListeners();
 
-
-        mQuantity_ed.addTextChangedListener(mQuantityMinMaxInput);
-        mQuantity_ed.addTextChangedListener(mQuantityInputWatcher);
-        mQuantity_slider.addOnChangeListener(mQuantitySliderListener);
 
     }
 
@@ -79,7 +117,29 @@ public class MainActivity extends AppCompatActivity {
     private void setHints() {
         Resources r = getResources();
         mQuantity_ed.setHint(r.getInteger(R.integer.min_value) + " - " + r.getInteger(R.integer.quantity_max_value));
+        mLength_ed.setHint(r.getInteger(R.integer.min_value) + " - " + r.getInteger(R.integer.length_max_value));
+        mWidth_ed.setHint(r.getInteger(R.integer.min_value) + " - " + r.getInteger(R.integer.width_max_value));
+        mThickness_ed.setHint(r.getInteger(R.integer.min_value) + " - " + r.getInteger(R.integer.thickness_max_value));
+    }
 
+    private void setWatchersAndListeners() {
+        mMaterail_autotv.setOnFocusChangeListener(mOnMaterialFocusChangeListener);
+
+        mQuantity_ed.addTextChangedListener(mQuantityMinMaxInput);
+        mQuantity_ed.addTextChangedListener(mQuantityInputWatchSlider);
+        mQuantity_slider.addOnChangeListener(mQuantitySliderListener);
+
+        mLength_ed.addTextChangedListener(mLengthMinMaxInput);
+        mLength_ed.addTextChangedListener(mLengthInputWatchSlider);
+        mLength_slider.addOnChangeListener(mLengthSliderListener);
+
+        mWidth_ed.addTextChangedListener(mWidthMinMaxInput);
+        mWidth_ed.addTextChangedListener(mWidthInputWatchSlider);
+        mWidth_slider.addOnChangeListener(mWidthSliderListener);
+
+        mThickness_ed.addTextChangedListener(mThicknessMinMaxInput);
+        mThickness_ed.addTextChangedListener(mThicknessInputWatchSlider);
+        mThickness_slider.addOnChangeListener(mThicknessSliderListener);
 
     }
 
